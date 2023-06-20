6773ecb11d155ebad5cf73519923e1b5345dc29a
Tender_is_a_sin <842561566@qq.com>
2023-06-20 20:07:52 +0800

    init

---
+++ src/main/java/com/zu_erforschen/shipping_order_system/ShippingOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zu_erforschen.shipping_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zu_erforschen/shipping_order_system/ShippingOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zu_erforschen.shipping_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
